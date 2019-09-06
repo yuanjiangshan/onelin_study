c96a70496f18b056aaf21203872e03b3ed88c9d9
kikisweety <952076372@qq.com>
2019-09-06 11:26:31 +0800

    ttt

--- 01.Client/01.wx/pages/house/house.js
+++ 01.Client/01.wx/pages/house/house.js
@@ -66,23 +66,37 @@ Page({
 
 
     imageSrc2: [{
-      id: 0,
-      title1: "Melbourne",
-      title2: "墨尔本",
-      url: "../../images/melbourne.jpg",
-    }, {
-      id: 1,
-      title1: "Canberra",
-      title2: "堪培拉",
+        id: 0,
+        title1: "Sydney",
+        title2: "悉尼",
+        url: "../../images/sydney.png",
+      },
+      {
+        id: 1,
+        title1: "Melbourne",
+        title2: "墨尔本",
+        url: "../../images/melbourne.jpg",
+      }, {
+        id: 2,
+        title1: "Canberra",
+        title2: "堪培拉",
         url: "../../images/canberra.jpg",
-    }, {
-      id: 2,
-      title1: "Brisbane",
-      title2: "布里斯班",
+      }, {
+        id: 3,
+        title1: "Brisbane",
+        title2: "布里斯班",
         url: "../../images/brisbane.jpg",
+      }
+    ],
+
+    imageSrc3: [{
+      id: 0,
+      title1: "Dublin",
+      title2: "都柏林",
+      url: "../../images/dublin.png",
     }],
 
-    
+
     topics: [{
         id: 0,
         title: "英国",
@@ -94,7 +108,89 @@ Page({
       {
         id: 2,
         title: "爱尔兰",
-      }],
+      }
+    ],
+
+    introduces: [{
+        id: 0,
+        src: "../../images/intro1.jpeg",
+        title1: "纽卡斯尔",
+        place: "IQ Collingwood",
+        price: "£113 起/周",
+        des: "纽卡网红学生公寓，200英镑限时返现"
+      },
+      {
+        id: 1,
+        src: "../../images/intro2.jpg",
+        title1: "哈德斯菲尔德",
+        place: "The Courtyard",
+        price: "£140 起/周",
+        des: "哈德斯菲尔德早鸟预订，火爆抢房"
+      },
+      {
+        id: 2,
+        src: "../../images/intro3.jpg",
+        title1: "谢菲尔德",
+        place: "New Era Student Living",
+        price: "£148 起/周",
+        des: "谢菲尔德最新Studio，限时优惠"
+      }, {
+        id: 3,
+        src: "../../images/intro4.jpg",
+        title1: "格拉斯哥",
+        place: "true Glasgow",
+        price: "£150 起/周",
+        des: "£250奢华床品厨具免费送+免费接机"
+      },
+      {
+        id: 4,
+        src: "../../images/intro3.jpg",
+        title1: "谢菲尔德",
+        place: "Fenton House",
+        price: "£130 起/周",
+        des: "限时折扣，立省300镑"
+      },
+      {
+        id: 5,
+        src: "../../images/intro5.jpg",
+        title1: "伯明翰",
+        place: "Vita Birmingham",
+        price: "£189 起/周",
+        des: "全新学生公寓，限时高额返现进行中"
+      },
+      {
+        id: 6,
+        src: "../../images/intro6.jpg",
+        title1: "利兹",
+        place: "Leeds St Albans",
+        price: "£182 起/周",
+        des: "利兹全新公寓，抢房进行中"
+      },
+      {
+        id: 7,
+        src: "../../images/intro7.jpg",
+        title1: "约克",
+        place: "VITA Student York",
+        price: "£194 起/周",
+        des: "拼多多，拼得多，省得多！"
+      },
+      {
+        id: 8,
+        src: "../../images/intro8.jpg",
+        title1: "曼切斯特",
+        place: "Circle Square",
+        price: "£255 起/周",
+        des: "限时高额返现，拼团更实惠"
+      },
+      {
+        id: 9,
+        src: "../../images/intro9.jpg",
+        title1: "伦敦",
+        place: "IQ Bankside",
+        price: "£351 起/周",
+        des: "限时200英镑返现"
+      }
+    ]
 
   },
 
@@ -111,7 +207,7 @@ Page({
     })
   },
   clickItem: function(res) {
-    console.log(res);
+    // console.log(res);
     let id = res.currentTarget.dataset.www;
     this.setData({
       indexSelect: id
